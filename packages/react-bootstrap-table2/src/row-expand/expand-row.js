c92db25f6828b7759356573a07d34eb918ceda7f
Dhika TB <dhikatb@gmail.com>
2019-11-19 10:34:05 +0700

    Update expand-row.js

--- packages/react-bootstrap-table2/src/row-expand/expand-row.js
+++ packages/react-bootstrap-table2/src/row-expand/expand-row.js
@@ -3,13 +3,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
 
-const ExpandRow = ({ children, expanded, onClosed, className, ...rest }) => (
+const ExpandRow = ({ children, expanded, onClosed, className, transitionTimeout, ...rest }) => (
   <tr>
     <td className={ cs('reset-expansion-style', className) } { ...rest }>
       <CSSTransition
         appear
         in={ expanded }
-        timeout={ 400 }
+        timeout={ transitionTimeout }
         classNames="row-expand-slide"
         onExited={ onClosed }
       >
@@ -27,14 +27,16 @@ ExpandRow.propTypes = {
   children: PropTypes.node,
   expanded: PropTypes.bool,
   onClosed: PropTypes.func,
-  className: PropTypes.string
+  className: PropTypes.string,
+  transitionTimeout: PropTypes.number
 };
 
 ExpandRow.defaultProps = {
   children: null,
   expanded: false,
   onClosed: null,
-  className: ''
+  className: '',
+  transitionTimeout: 400
 };
 
 export default ExpandRow;
